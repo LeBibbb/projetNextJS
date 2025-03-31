3db79b5e41112c5fab999cf686872c0abfa14cd0
LeBibbb*-96+3 <ibiberisha02@gmail.com>
2025-03-31 15:16:59 +0200

    counetr

--- src/lib/store.jsx
+++ src/lib/store.jsx
@@ -1,5 +1,4 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "./slices/counterSlice"; 
 import cartSlice from "./slices/cartSlice";
 const store = configureStore({
   reducer: {
